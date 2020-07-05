833f966356135154401be07eceba28c4eeacdcf7
sudipto-me <sudipto004.ruet@gmail.com>
2020-07-05 19:32:24 +0600

    text domain added

--- includes/admin/settings/wc-minmax-quantities-settings-help.php
+++ includes/admin/settings/wc-minmax-quantities-settings-help.php
@@ -19,7 +19,7 @@ class WC_Minmax_Quantities_Settings_Help extends WC_Settings_Page {
 	 */
 	public function __construct() {
 		$this->id    = 'help';
-		$this->label = __( 'Help', 'wc_crm' );
+		$this->label = __( 'Help', 'wc-minmax-quantities' );
 		add_filter('wc_minmax_quantities_settings_tabs_array', array($this, 'add_settings_page'), 99);
 		add_action('wc_minmax_quantities_settings_' . $this->id, array($this, 'output'));
 		add_action('wc_minmax_quantities_settings_save_' . $this->id, array($this, 'save'));
