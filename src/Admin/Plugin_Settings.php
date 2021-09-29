8b1b2199a349a3a8aac809255466c5fea2429da9
sudipto-me <sudipto004.ruet@gmail.com>
2021-09-29 11:32:13 +0600

    text domain updated

--- src/Admin/Plugin_Settings.php
+++ src/Admin/Plugin_Settings.php
@@ -30,7 +30,7 @@ class Plugin_Settings extends Framework\Admin\Settings_Page {
 	 * @since 1.0.0
 	 */
 	protected function get_page_title() {
-		return esc_html__( 'WC Min Max Settings', 'wc-min-max-qunatities' );
+		return esc_html__( 'WC Min Max Settings', 'wc-min-max-quantities' );
 	}
 
 	/**
@@ -53,8 +53,8 @@ class Plugin_Settings extends Framework\Admin\Settings_Page {
 	public function register_admin_menu() {
 		add_submenu_page(
 			'woocommerce',
-			__( 'Min Max Quantities', 'wc-min-max-qunatities' ),
-			__( 'Min Max Quantities', 'wc-min-max-qunatities' ),
+			__( 'Min Max Quantities', 'wc-min-max-quantities' ),
+			__( 'Min Max Quantities', 'wc-min-max-quantities' ),
 			'manage_options',
 			$this->get_page_slug(),
 			array( $this, 'display_settings_page' )
@@ -71,83 +71,69 @@ class Plugin_Settings extends Framework\Admin\Settings_Page {
 	protected function get_settings() {
 		$settings = array(
 			'general' => array(
-				'title'    => __( 'General', 'wc-min-max-qunatities' ),
+				'title'    => __( 'General', 'wc-min-max-quantities' ),
 				'sections' => array(
 					'main' => array(
-						'title'  => __( 'General', 'wc-min-max-qunatities' ),
+						'title'  => __( 'General', 'wc-min-max-quantities' ),
 						'fields' => array(
 							array(
-								'title' => esc_html__( 'Product Restrictions', 'wc-min-max-qunatities' ),
+								'title' => esc_html__( 'Product Restrictions', 'wc-min-max-quantities' ),
 								'type'  => 'section',
-								'desc'  => esc_html__( 'The following options are for adding minimum maximum rules for products globally.', 'wc-min-max-qunatities' ),
+								'desc'  => esc_html__( 'The following options are for adding minimum maximum rules for products globally.', 'wc-min-max-quantities' ),
 								'id'    => 'section_product_restrictions',
 							),
 							array(
-								'title'   => esc_html__( 'Minimum product quantity', 'wc-min-max-qunatities' ),
+								'title'   => esc_html__( 'Minimum product quantity', 'wc-min-max-quantities' ),
 								'id'      => 'min_product_quantity',
-								'desc'    => esc_html__( 'Minimum number of items required for each product. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'    => esc_html__( 'Minimum number of items required for each product. Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'    => 'number',
 								'default' => '0',
 							),
 							array(
-								'title'   => esc_html__( 'Maximum product quantity', 'wc-min-max-qunatities' ),
+								'title'   => esc_html__( 'Maximum product quantity', 'wc-min-max-quantities' ),
 								'id'      => 'max_product_quantity',
-								'desc'    => esc_html__( 'Maximum quantity allowed for each single product. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'    => esc_html__( 'Maximum quantity allowed for each single product. Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'    => 'number',
 								'default' => '0',
 							),
 							array(
-								'title'   => esc_html__( 'Minimum product total', 'wc-min-max-qunatities' ),
-								'id'      => 'min_product_total',
-								'desc'    => esc_html__( 'Minimum price total of items required for each product. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
-								'type'    => 'number',
-								'default' => '0',
-							),
-							array(
-								'title'   => esc_html__( 'Maximum product total', 'wc-min-max-qunatities' ),
-								'id'      => 'max_product_total',
-								'desc'    => esc_html__( 'Maximum allowed price total of items for each product. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
-								'type'    => 'number',
-								'default' => '0',
-							),
-							array(
-								'title'   => esc_html__( 'Quantity groups of', 'wc-min-max-qunatities' ),
+								'title'   => esc_html__( 'Quantity groups of', 'wc-min-max-quantities' ),
 								'id'      => 'product_quantity_step',
-								'desc'    => esc_html__( 'Enter a quantity to only allow product to be purchased in groups of X. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'    => esc_html__( 'Enter a quantity to only allow product to be purchased in groups of X. Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'    => 'number',
 								'default' => '0',
 							),
 							array(
-								'title' => esc_html__( 'Order Restriction', 'wc-min-max-qunatities' ),
+								'title' => esc_html__( 'Order Restriction', 'wc-min-max-quantities' ),
 								'type'  => 'section',
-								'desc'  => esc_html__( 'The following options are for cart restrictions', 'wc-min-max-qunatities' ),
+								'desc'  => esc_html__( 'The following options are for cart restrictions', 'wc-min-max-quantities' ),
 								'id'    => 'cart_restrictions',
 							),
 							array(
-								'title' => esc_html__( 'Minimum order quantity', 'wc-min-max-qunatities' ),
+								'title' => esc_html__( 'Minimum order quantity', 'wc-min-max-quantities' ),
 								'id'    => 'min_order_quantity',
-								'desc'  => esc_html__( 'Minimum number of items in cart. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'  => esc_html__( 'Minimum number of items in cart. Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'  => 'number',
 								'min'   => 0,
 							),
 							array(
-								'title' => esc_html__( 'Maximum order quantity', 'wc-min-max-qunatities' ),
+								'title' => esc_html__( 'Maximum order quantity', 'wc-min-max-quantities' ),
 								'id'    => 'max_order_quantity',
-								'desc'  => esc_html__( 'Maximum number of items in cart. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'  => esc_html__( 'Maximum number of items in cart. Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'  => 'number',
 								'min'   => 0,
 							),
 							array(
-								'title'   => esc_html__( 'Minimum order amount', 'wc-min-max-qunatities' ),
+								'title'   => esc_html__( 'Minimum order amount', 'wc-min-max-quantities' ),
 								'id'      => 'min_order_amount',
-								'desc'    => esc_html__( 'Minimum order total. Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'    => esc_html__( 'Minimum order total. Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'    => 'number',
 								'default' => '0',
 							),
 							array(
-								'title'   => esc_html__( 'Maximum order amount', 'wc-min-max-qunatities' ),
+								'title'   => esc_html__( 'Maximum order amount', 'wc-min-max-quantities' ),
 								'id'      => 'max_order_amount',
-								'desc'    => esc_html__( 'Maximum order total.Set zero for no restrictions.', 'wc-min-max-qunatities' ),
+								'desc'    => esc_html__( 'Maximum order total.Set zero for no restrictions.', 'wc-min-max-quantities' ),
 								'type'    => 'number',
 								'default' => '0',
 							),
