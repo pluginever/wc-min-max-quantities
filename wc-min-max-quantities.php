c57c9a4a72e4b02421603e5eb7a5b6232c374eba
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-09-02 16:50:06 +0600

    Add optimized autoloader

--- wc-min-max-quantities.php
+++ wc-min-max-quantities.php
@@ -36,35 +36,8 @@ use WooCommerceMinMaxQuantities\Plugin;
 
 defined( 'ABSPATH' ) || exit;
 
-// Autoload function.
-spl_autoload_register(
-	function ( $class_name ) {
-		$prefix = 'WooCommerceMinMaxQuantities\\';
-		$len    = strlen( $prefix );
-		// Bail out if the class name doesn't start with our prefix.
-		if ( strncmp( $prefix, $class_name, $len ) !== 0 ) {
-			return;
-		}
-
-		// Remove the prefix from the class name.
-		$relative_class = substr( $class_name, $len );
-		// Replace the namespace separator with the directory separator.
-		$file = str_replace( '\\', DIRECTORY_SEPARATOR, $relative_class ) . '.php';
-
-		// Look for the file in the src and lib directories.
-		$file_paths = array(
-			__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $file,
-			__DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . $file,
-		);
-
-		foreach ( $file_paths as $file_path ) {
-			if ( file_exists( $file_path ) ) {
-				require_once $file_path;
-				break;
-			}
-		}
-	}
-);
+// Autoload classes.
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * Returns the main instance of plugin.
