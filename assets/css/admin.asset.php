d29ca2ecc5cfe42e8f70ffc84d8474df4ce3d620
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-09-02 15:10:06 +0600

    Fix #167

---
+++ assets/css/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4a44ffb22ebd9b0a0ab6');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = [
@@ -6,19 +8,52 @@ module.exports = [
         ...defaultConfig,
         entry: {
             ...defaultConfig.entry(),
-            'css/admin': './assets/src/css/admin.scss',
+            'css/admin': './.assets/css/admin.scss',
         },
         output: {
             ...defaultConfig.output,
             filename: '[name].js',
-            path: __dirname + '/assets/dist/',
+            path: __dirname + '/assets/',
         },
-        plugins: [
-            ...defaultConfig.plugins,
-            new RemoveEmptyScriptsPlugin({
-                stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
-                remove: /\.(js)$/,
-            }),
-        ],
-    },
+		module: {
+			rules: [
+				...defaultConfig.module.rules,
+				{
+					test: /\.svg$/,
+					issuer: /\.(j|t)sx?$/,
+					use: ['@svgr/webpack', 'url-loader'],
+					type: 'javascript/auto',
+				},
+				{
+					test: /\.svg$/,
+					issuer: /\.(sc|sa|c)ss$/,
+					type: 'asset/inline',
+				},
+				{
+					test: /\.(bmp|png|jpe?g|gif)$/i,
+					type: 'asset/resource',
+					generator: {
+						filename: 'images/[name].[hash:8][ext]',
+					},
+				},
+			],
+		},
+		plugins: [
+			...defaultConfig.plugins,
+			// Copy images to the build folder.
+			// new CopyWebpackPlugin({
+			// 	patterns: [
+			// 		{
+			// 			from: path.resolve(__dirname, '.assets/images'),
+			// 			to: path.resolve(__dirname, 'assets/images'),
+			// 		}
+			// 	]
+			// }),
+
+			new RemoveEmptyScriptsPlugin({
+				stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
+				remove: /\.(js)$/,
+			}),
+		],
+	},
 ];
