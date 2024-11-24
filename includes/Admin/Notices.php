353791d7f7c8f8635f76247a06609a45f3d6e29f
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-11-24 16:20:07 +0600

    Fix #184

--- includes/Admin/Notices.php
+++ includes/Admin/Notices.php
@@ -30,16 +30,16 @@ class Notices {
 		$installed_time = get_option( 'wc_min_max_quantities_installed' );
 		$current_time   = wp_date( 'U' );
 
-		// Halloween's promotion notice.
-		$halloween_time = date_i18n( strtotime( '2024-11-11 00:00:00' ) );
-		if ( $current_time < $halloween_time ) {
+		// Promotional notice for Black Friday.
+		$offer_time = date_i18n( strtotime( '2024-12-07 00:00:00' ) );
+		if ( $current_time < $offer_time ) {
 			wc_min_max_quantities()->notices->add(
 				array(
-					'message'     => __DIR__ . '/views/notices/halloween.php',
+					'message'     => __DIR__ . '/views/notices/black-friday.php',
+					'notice_id'   => 'wcmmq_black_friday_promotion_24',
+					'class'       => 'notice-black-friday',
+					'style'       => 'border-left-color: #2271b1;',
 					'dismissible' => false,
-					'notice_id'   => 'wcmmq_halloween_promotion',
-					'style'       => 'border-left-color: #8500ff;background-image: url("' . esc_url( wc_min_max_quantities()->get_assets_url( 'images/halloween-banner.svg' ) ) . '");',
-					'class'       => 'notice-halloween',
 				)
 			);
 		}
@@ -49,7 +49,7 @@ class Notices {
 				array(
 					'message'     => __DIR__ . '/views/notices/upgrade.php',
 					'notice_id'   => 'wcmmq_upgrade',
-					'style'       => 'border-left-color: #0542fa;',
+					'style'       => 'border-left-color:#0542fa;',
 					'dismissible' => false,
 				)
 			);
---
+++ includes/Admin/views/notices/black-friday.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Admin notice: Black Friday offer.
+ *
+ * @package WooCommerceMinMaxQuantities
+ * @since 2.0.2
+ * @return void
+ */
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+?>
+<div class="notice-body">
+	<div class="notice-icon">
+		<img src="<?php echo esc_url( wc_min_max_quantities()->get_assets_url( 'images/black-friday-icon.svg' ) ); ?>" alt="WooCommerce Min-Max Quantities Black Friday Offer">
+	</div>
+	<div class="notice-content">
+		<h3>
+			<?php esc_html_e( 'Black Friday & Cyber Monday: Flat 40% OFF All Premium Plugins!', 'wc-min-max-quantities' ); ?>
+		</h3>
+		<p>
+			<?php
+			echo wp_kses_post(
+				sprintf(
+					// translators: 1.Offer Percentage, 2. Coupon Code.
+					__( 'Boost your WooCommerce store this Black Friday! Get %1$s on all premium plugins with code %2$s. Don’t miss this limited-time deal!', 'wc-min-max-quantities' ),
+					'<strong>' . esc_attr( '40% OFF' ) . '</strong>',
+					'<strong>' . esc_attr( 'FLASH40' ) . '</strong>'
+				)
+			);
+			?>
+		</p>
+	</div>
+</div>
+<div class="notice-footer">
+	<div class="footer-btn">
+		<a href="<?php echo esc_url( wc_min_max_quantities()->plugin_uri . '?utm_source=plugin&utm_medium=notice&utm_campaign=black-friday-2024&discount=FLASH40' ); ?>" class="button button-primary black-friday-upgrade-btn" target="_blank">
+			<span class="dashicons dashicons-cart"></span>
+			<?php esc_html_e( 'Claim your 40% discount!!', 'wc-min-max-quantities' ); ?>
+		</a>
+		<a href="#" data-snooze="<?php echo esc_attr( WEEK_IN_SECONDS ); ?>">
+			<span class="dashicons dashicons-clock"></span>
+			<?php esc_html_e( 'Remind me later', 'wc-min-max-quantities' ); ?>
+		</a>
+		<a href="#" class="button black-friday-dismiss-btn" data-dismiss>
+			<span class="dashicons dashicons-dismiss"></span>
+			<span class="btn-text"><?php esc_html_e( 'DISMISS', 'wc-min-max-quantities' ); ?></span>
+		</a>
+	</div>
+	<span class="black-friday-footer-text"><?php esc_html_e( 'Valid until November 7, 2024', 'wc-min-max-quantities' ); ?></span>
+</div>
