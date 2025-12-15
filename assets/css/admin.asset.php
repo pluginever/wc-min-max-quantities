c1ecbb39038910f0513cd64e71ad5d755e98baba
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-12-15 17:10:38 +0600

    Add Trunk and update dev tools

---
+++ assets/css/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5431916bbca227e0f6d8');
--- wc-min-max-quantities.php
+++ wc-min-max-quantities.php
@@ -5,39 +5,43 @@
  * Description:          The plugin allows you to Set minimum and maximum allowable product quantities and price per product and order.
  * Version:              2.2.2
  * Requires at least:    5.2
+ * Tested up to:         6.9
  * Requires PHP:         7.4
  * Author:               PluginEver
  * Author URI:           https://pluginever.com/
- * Text Domain:          wc-min-max-quantities
- * Domain Path:          /languages
  * License:              GPL v2 or later
  * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
- * Tested up to:         6.8
+ * Text Domain:          wc-min-max-quantities
+ * Domain Path:          /languages
  * WC requires at least: 3.0.0
- * WC tested up to:      10.3
+ * WC tested up to:      10.4
  * Requires Plugins:     woocommerce
  *
- * @package WooCommerceMinMaxQuantities
- * @author  PluginEver
- * @link    https://pluginever.com/plugins/woocommerce-min-max-quantities-pro/
+ * @link                 https://pluginever.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ * @author              Sultan Nasir Uddin <manikdrmc@gmail.com>
+ * @copyright           2025 ByteEver
+ * @license             GPL-2.0+
+ * @package             WooCommerceMinMaxQuantities
  */
 
-// Don't call the file directly.
 defined( 'ABSPATH' ) || exit;
 
-// Require the autoloader.
+// Autoloader.
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/libraries/autoload.php';
 
 // Instantiate the plugin.
 WooCommerceMinMaxQuantities\Plugin::create(
