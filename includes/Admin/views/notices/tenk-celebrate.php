3ead48736fbbeaa362f21bb2d0d66ecb433b38ed
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2026-01-18 17:53:12 +0600

    Fixed few issues

--- includes/Admin/views/notices/tenk-celebrate.php
+++ includes/Admin/views/notices/tenk-celebrate.php
@@ -12,16 +12,16 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="notice-body">
 	<div class="notice-icon">
-		<img src="<?php echo esc_attr( wc_min_max_quantities()->get_assets_url( 'images/plugin-icon.png' ) ); ?>" alt="Min Max Quantities">
+		<img src="<?php echo esc_attr( wc_min_max_quantities()->get_assets_url( 'images/plugin-icon.png' ) ); ?>" alt="<?php esc_attr_e( 'Min Max Quantities', 'wc-min-max-quantities' ); ?>" />
 	</div>
 	<div class="notice-content">
-		<h3><?php esc_attr_e( '10,000+ Users Celebration!', 'wc-min-max-quantities' ); ?></h3>
+		<h3><?php esc_html_e( '10,000+ Users Celebration!', 'wc-min-max-quantities' ); ?></h3>
 		<p>
 			<?php
 			echo wp_kses_post(
 				sprintf(
 				// translators: %1$s: Min Max Quantities Pro link, %2$s: Coupon code.
-					__( '🎉 We hit 10,000 users! To celebrate with you, enjoy <strong>20%% discount</strong> OFF on  %1$s our Premium plan. Use code: %2$s at checkout to grab the deal.', 'wc-min-max-quantities' ),
+					__( '🎉 We hit 10,000+ users! To celebrate with you, enjoy <strong>20%% discount</strong> OFF on %1$s our Premium plan. Use code: %2$s at checkout to grab the deal.', 'wc-min-max-quantities' ),
 					'<a href="https://pluginever.com/plugins/woocommerce-min-max-quantities-pro/?utm_source=plugin-notice&utm_medium=admin-notice&utm_campaign=10k-celebration&utm_id=10k-celebration" target="_blank"><strong>Min Max Quantities Pro</strong></a>',
 					'<strong>THANKYOU10K</strong>'
 				)
